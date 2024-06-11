666abb9404d70e44425039dae94990bd2841316f
CHINHAE <675351444@qq.com>
2024-06-11 19:28:23 +0800

    asd

---
+++ day-01/src/com/woniuxy/test/Exercise1.java
@@ -0,0 +1,20 @@
+package com.woniuxy.test;
+
+/**
+ * 这个类是：
+ *
+ * @author: CHINHAE
+ * @date: 2024/6/11 17:39
+ * @version: 1.0
+ */
+public class Exercise1 {
+    public static void main(String[] args){
+        int i = 10;
+        int j = 3;
+        int imp = i;
+        i = j;
+        j = imp;
+    System.out.println(i);
+    System.out.println(j);
+    }
+}
---
+++ day-01/src/com/woniuxy/test/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.woniuxy.test;
+
+/**
+ * 这个类是：
+ *
+ * @author: CHINHAE
+ * @date: 2024/6/11 14:51
+ * @version: 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * 第一个java程序,Hello World!
+     * */
+    public static void main(String[] args){
+    System.out.println("Hello World!");
+    }
+}
